d23a5eb2158c00263c2705d14665f930ce8beea6
nguyenthanhthang <nguyenthanhthang999@gmail.com>
2024-08-20 00:24:08 +0700

    update event active section and navlink on scroll, new event create navbar

--- js/app.js
+++ js/app.js
@@ -1,16 +1,62 @@
-document.addEventListener('DOMContentLoaded', function() {
+/**
+ * @description Create navbar
+ */
+function createNavbar() {
+    const header = document.querySelector('header');
+    const sections = document.querySelectorAll('section');
+    const navBar = document.createElement('nav');
+    const navList = document.createElement('ul');
+
+    sections.forEach((section, index) => {
+        const navItem = document.createElement('li');
+        const navLink = document.createElement('a');
+
+        navLink.href = `#${section.id}`;
+        navLink.textContent = `Section ${index + 1}`;
+        navLink.classList.add('nav-link');
+
+        navItem.appendChild(navLink);
+        navList.appendChild(navItem);
+    });
+
+    navBar.classList.add('wide');
+    navBar.appendChild(navList);
+
+    header.appendChild(navBar);
+}
+
+/**
+ * @description Activate Section and navlink On Scroll
+ */
+function activeSectionAndNavLinkOnScroll() {
     const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('.nav-link');
 
-    function activateLink() {
-        let index = sections.length;
-    
-        while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
-        
-        navLinks.forEach((link) => link.classList.remove('active'));
-        navLinks[index].classList.add('active');
+    function checkSection() {
+        sections.forEach((section) => {
+            const locate = section.getBoundingClientRect();
+
+            if(locate.top >= 0 && locate.top <= window.innerHeight/2) {
+                sections.forEach((sec) => sec.classList.remove('active'));
+                //Active section
+                section.classList.add('active');
+
+                //Active link
+                navLinks.forEach((link) => {
+                    link.classList.remove('active');
+                    if(link.getAttribute('href') === `#${section.id}`) {
+                        link.classList.add('active');
+                    }
+                });
+            }
+        });
     }
-    
-    activateLink(); // Kích hoạt nút khi tải trang
-    window.addEventListener('scroll', activateLink);
+
+    checkSection();
+    window.addEventListener('scroll', checkSection);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    createNavbar();
+    activeSectionAndNavLinkOnScroll();
 });
