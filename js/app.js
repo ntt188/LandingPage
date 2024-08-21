0c6faeb99f6e585b3769150e42a5ae94298e290a
nguyenthanhthang <nguyenthanhthang999@gmail.com>
2024-08-22 00:31:52 +0700

    reponsive landingpage

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let timeout; //param time hide scroll
+
 /**
  * @description Create navbar
  */
@@ -56,7 +58,79 @@ function activeSectionAndNavLinkOnScroll() {
     window.addEventListener('scroll', checkSection);
 }
 
+/**
+ * @description Add navbar button on mobile screen
+ */
+function addButtonNavBar() {
+    const header = document.querySelector('header');
+    const nav = document.querySelector('nav');
+
+    if (window.innerWidth <= 739) {
+        const btnNav = document.createElement('label');
+        btnNav.classList.add('nav-btn');
+
+        const icon = document.createElement('i');
+        icon.classList.add('fa', 'fa-bars', 'fa-3x');
+        icon.setAttribute('aria-hidden', 'true');
+
+        btnNav.appendChild(icon);
+
+        header.appendChild(btnNav);
+
+        btnNav.addEventListener('click', function() {
+            icon.classList.toggle('fa-bars');
+            icon.classList.toggle('fa-times');
+            nav.classList.toggle('open');
+        });
+    }
+}
+
+/**
+ * @description add class 'scrollbar' to body to show scrollbar
+ */
+function showScrollbar() {
+    document.body.classList.add('scrollbar');
+}
+
+/**
+ * @description remove class 'scrollbar' to body to hide scrollbar
+ */
+function hideScrollbar() {
+    document.body.classList.remove('scrollbar');
+}
+
+/**
+ * @description function to set timeout when user operates
+ */
+function handleUserActivity() {
+    clearTimeout(timeout);
+    showScrollbar();
+    timeout = setTimeout(hideScrollbar, 1500);
+}
+
+/**
+ * @description Show Hide Scroll
+ */
+function hideShowScrollbar() {
+    window.addEventListener('scroll', handleUserActivity);
+    hideScrollbar();
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     createNavbar();
     activeSectionAndNavLinkOnScroll();
+    addButtonNavBar();
+    hideShowScrollbar();
+});
+
+/**
+ * @description Remove or Add navbar button when resize
+ */
+window.addEventListener('resize', function(){
+    const btnNav = document.querySelector('.nav-btn');
+    if (btnNav && this.window.innerWidth > 739) {
+        btnNav.remove();
+    } else if (!btnNav && this.window.innerWidth <= 739) {
+        addButtonNavBar();
+    }
 });
